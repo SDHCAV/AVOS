7210284a3f5f700be025af8354d0417496d5adeb
Jasmine <jlau51@ucmerced.edu>
2026-08-12 17:17:30 -0700

    setup C++ backend with CMake

---
+++ engine/src/main.cpp
@@ -0,0 +1,11 @@
+int main(){
+    return 0;
+}
+
+//run the following to build toolchain and confirm compiler, CMake, and folder strucutre correctly wired before adding JUICE dependency
+/*
+cd engine
+mkdir build && cd build
+cmake ..
+cmake --build .
+*/
