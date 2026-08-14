912bff95cf23bd91b59c82ea08fcf1437a8cdac2
Jasmine <jlau51@ucmerced.edu>
2026-08-13 17:13:10 -0700

    JUICE integration

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,11 +1,28 @@
-int main(){
+#include <juce_audio_devices/juce_audio_devices.h>
+
+int main() {
+    juce::ScopedJuceInitialiser_GUI gui;
+
+    juce::AudioDeviceManager manager;
+    manager.initialiseWithDefaultDevices(2, 2);
+
+    auto* type = manager.getCurrentDeviceTypeObject();
+
+    if (type != nullptr) {
+        auto deviceNames = type->getDeviceNames();
+        juce::Logger::writeToLog("--- Available Audio Devices ---");
+        for (const auto& name : deviceNames) {
+            juce::Logger::writeToLog(name);
+        }
+    }
     return 0;
 }
 
 //run the following to build toolchain and confirm compiler, CMake, and folder strucutre correctly wired before adding JUICE dependency
 /*
-cd engine
-mkdir build && cd build
+cd engine/build
+rm -rf *
 cmake ..
 cmake --build .
+
 */
